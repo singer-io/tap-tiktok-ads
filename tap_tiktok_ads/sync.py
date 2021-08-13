56517d5fae3614e324b0446f342535bde6ae67ee
João Romano Barbosa <joao.romano@gruvi.tv>
2021-08-13 15:43:15 +0100

    Sync now supports date ranges higher than 30 days. And automatically gets data extrated until present day if end_date is not present in config file.

--- tap_tiktok_ads/sync.py
+++ tap_tiktok_ads/sync.py
@@ -1,13 +1,61 @@
-import json
+from datetime import timedelta
 
 import singer
+from dateutil.parser import parse
 from singer.utils import now
+from singer import Transformer, UNIX_MILLISECONDS_INTEGER_DATETIME_PARSING, metadata
 
 from tap_tiktok_ads import TikTokClient
 
 LOGGER = singer.get_logger()
 
-def sync_with_endpoint(client: TikTokClient, config, state, catalog, endpoint_config):
+def get_date_batches(config):
+    if 'start_date' in config:
+        start_date = parse(config['start_date'])
+
+    if 'end_date' in config:
+        end_date = parse(config['end_date'])
+    else:
+        end_date = now()
+
+    date_batches = []
+    if end_date.date() >= start_date.date():
+        while(start_date < end_date):
+            next_batch = start_date + timedelta(days=29)
+            date_batch = {}
+            date_batch['start_date'] = start_date
+            date_batch['end_date'] = end_date if next_batch > end_date else next_batch
+            date_batches.append(date_batch)
+            start_date = next_batch + timedelta(days=1)
+        return date_batches
+    raise ValueError('end_date must not be greater than start_date')
+
+def process_batch(stream, records):
+    bookmark_column = stream.replication_key[0]
+    sorted_records = sorted(records, key=lambda x: x['dimensions']['stat_time_day'])
+
+    singer.write_schema(
+        stream_name=stream.tap_stream_id,
+        schema=stream.schema.to_dict(),
+        key_properties=stream.key_properties,
+    )
+
+    max_bookmark = None
+    for record in sorted_records:
+        # TODO: transform secondary-objective fields into correct type
+        with Transformer(integer_datetime_fmt=UNIX_MILLISECONDS_INTEGER_DATETIME_PARSING) as transformer:
+            usable_data = record['metrics'] | record['dimensions']
+            usable_data['updated_at'] = usable_data.pop('stat_time_day')
+
+            transformed_record = transformer.transform(usable_data, stream.schema.to_dict(),
+                                                       metadata.to_map(stream.metadata))
+            # write one or more rows to the stream:
+            singer.write_record(stream.tap_stream_id, transformed_record)
+            if bookmark_column:
+                # update bookmark to latest value
+                singer.write_state({stream.tap_stream_id: transformed_record[bookmark_column]})
+                
+def sync_with_endpoint(client: TikTokClient, config, state, stream, endpoint_config):
     records = []
     total_records = 0
     page = 1
@@ -15,9 +63,6 @@ def sync_with_endpoint(client: TikTokClient, config, state, catalog, endpoint_co
     headers = {
         "Access-Token": config['access_token']
     }
-
-    # Todo: Calculate the number of API calls to do within date interval,
-    #  since TikTok only supports an interval of 30 days
 
     while (len(records) < total_records) or (page == 1):
         endpoint_config['params']['page'] = page
@@ -27,16 +72,15 @@ def sync_with_endpoint(client: TikTokClient, config, state, catalog, endpoint_co
             records = records + response['data']['list']
         page = page + 1
 
-    return records
+    process_batch(stream, records)
 
 def sync(client, config, state, catalog):
     """ Sync data from tap source """
-    if 'start_date' in config:
-        start_date = config['start_date']
 
-    if 'end_date' in config:
-        end_date = config['end_date']
-    end_date = now()
+    # Each API call to TikTok with a stat_time_day dimension only support a range
+    # of 30 days. Because of this we need to separate the interval between start_date
+    # and end_date into batches of 30 days max.
+    date_batches = get_date_batches(config)
 
     endpoints = {
         "auction_ad_reports": {
@@ -88,9 +132,8 @@ def sync(client, config, state, catalog):
                         "follows",
                         "clicks_on_music_disc"
                     ]""",
-            "start_date": config["start_date"][:10],
-            "end_date": config["end_date"][:10],
-            "page_size": 50
+                "page_size": 50,
+                "lifetime": "false"
             },
             "id-fields": ["ad_id", "adgroup_id", "campaign_id", "updated_at"]
         }
@@ -99,37 +142,11 @@ def sync(client, config, state, catalog):
     # Loop over selected streams in catalog
     for stream in catalog.get_selected_streams(state):
         LOGGER.info("Syncing stream:" + stream.tap_stream_id)
-        all_records = []
-
-        for stream_name, endpoint_config in endpoints.items():
-            if 'accounts' in config and endpoint_config['req_advertiser_id']:
-                for ad_account in config.get('accounts'):
-                    endpoint_config['params']['advertiser_id'] = ad_account
-                    all_records = sync_with_endpoint(client, config, state, catalog, endpoint_config)
-
-        bookmark_column = stream.replication_key
-        all_records = sorted(all_records, key=lambda x: (x['dimensions']['stat_time_day'], x['dimensions']['ad_id']))
-        is_sorted = True
-
-        singer.write_schema(
-            stream_name=stream.tap_stream_id,
-            schema=stream.schema.to_dict(),
-            key_properties=stream.key_properties,
-        )
-
-        # TODO: delete and replace this inline function with your own data retrieval process:
-        tap_data = lambda: [{"id": x, "name": "row${x}"} for x in range(1000)]
-
-        max_bookmark = None
-        for row in tap_data():
-            # TODO: place type conversions or transformations here
-
-            # write one or more rows to the stream:
-            singer.write_records(stream.tap_stream_id, [row])
-            if bookmark_column:
-                    # update bookmark to latest value
-                    singer.write_state({stream.tap_stream_id: row[bookmark_column]})
-
-        if bookmark_column and not is_sorted:
-            singer.write_state({stream.tap_stream_id: max_bookmark})
-    return
+        endpoint_config = endpoints[stream.tap_stream_id]
+        if 'accounts' in config and endpoint_config['req_advertiser_id']:
+            for ad_account in config.get('accounts'):
+                endpoint_config['params']['advertiser_id'] = ad_account
+                for date_batch in date_batches:
+                    endpoint_config['params']['start_date'] = date_batch['start_date'].date()
+                    endpoint_config['params']['end_date'] = date_batch['end_date'].date()
+                    sync_with_endpoint(client, config, state, stream, endpoint_config)
