00f675726bea8cb9a0d3fd48cbfb8e873a1c2665
namrata270998 <namrata.brahmbhatt@crestdatasys.com>
2022-03-08 13:38:37 +0000

    initial commit

--- tap_tiktok_ads/__init__.py
+++ tap_tiktok_ads/__init__.py
@@ -16,6 +16,7 @@ def main():
     args = utils.parse_args(REQUIRED_CONFIG_KEYS)
 
     with TikTokClient(access_token=args.config['access_token'],
+                      sandbox=args.config.get('sandbox', False),
                       user_agent=args.config['user_agent']) as tik_tok_client:
 
         # If discover flag was passed, run discovery mode and dump output to stdout
