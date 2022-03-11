9595745b84482f4c49da679ff2380616ddd3a57a
harshpatel4_crest <harsh.patel4@crestdatasys.com>
2022-03-11 18:09:25 +0530

    added tap tester tests

--- tap_tiktok_ads/__init__.py
+++ tap_tiktok_ads/__init__.py
@@ -16,7 +16,8 @@ def main():
     args = utils.parse_args(REQUIRED_CONFIG_KEYS)
 
     with TikTokClient(access_token=args.config['access_token'],
-                      user_agent=args.config['user_agent']) as tik_tok_client:
+                      user_agent=args.config['user_agent'],
+                      sandbox=args.config.get('sandbox', False)) as tik_tok_client:
 
         # If discover flag was passed, run discovery mode and dump output to stdout
         if args.discover:
