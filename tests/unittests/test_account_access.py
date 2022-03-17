66b2076eae2f128902688973c2534c13e7045e99
namrata270998 <namrata.brahmbhatt@crestdatasys.com>
2022-03-17 08:49:21 +0000

    handled service error in check account access and unittsts

--- tests/unittests/test_account_access.py
+++ tests/unittests/test_account_access.py
@@ -63,3 +63,25 @@ class TestErrorHandling(unittest.TestCase):
         verified = client.check_access_token()
         # Verify the check_access_token() returns true
         self.assertEqual(verified, True)
+
+    def test_check_access_token_service_error(self, mocked_request):
+        """
+            Verify we raise error with custom error message while checking account access via 'check_access_token' method
+        """
+        # set config
+        config = {
+            "access_token": "test_access_token",
+            "user_agent": "test_user_agent",
+            "accounts": ['account_id1', 'account_id2']
+        }
+
+        # mock request and raise error
+        mocked_request.side_effect = [get_response(200, {"code": 0, "message": "OK"}), get_response(200, {"code": 51008, "message": "Service error:"})]
+        
+        # create client and call function
+        client = TikTokClient(config.get("access_token"), config.get('accounts'), config.get("user_agent"))
+        # verify that we raise TikTokAdsClientError error when using "with" statement
+        with self.assertRaises(TikTokAdsClientError) as e:
+            client.__enter__()
+        # verify the error is raised as expected with message
+        self.assertEqual(str(e.exception), "Error encountered accessing the accounts with the given account ids. Kindly check your account ids.")
